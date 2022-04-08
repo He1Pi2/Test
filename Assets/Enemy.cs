36858f249886357f50f63f6037b4efdc5586abda
He1Pi2 <97427725+He1Pi2@users.noreply.github.com>
2022-04-08 17:01:38 +1000

    боты сделаны

---
+++ Assets/Enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();    
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
